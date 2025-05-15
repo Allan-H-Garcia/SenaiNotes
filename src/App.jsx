c71127749dc0a8842b3a10c7de8d696f0f92faa9
Jaquelline <cjaquelliness@gmail.com>
2025-05-14 21:31:09 -0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/login";
 import Chat from "./pages/chat";
 import NewUser from "./pages/new-user/index";
 import ForgotPassword from "./pages/forgot/index";
-import ConfirPassword from "./pages/reset-password";
+import ConfirmPassword from "./pages/reset-password";
 
 
 
@@ -34,7 +34,7 @@ if (token == null){
         <Route path= "*" element= {<h1>Not found </h1>}></Route>
         <Route path="/new-user" element={<NewUser/>}></Route>
         <Route path="/forgot" element={<ForgotPassword/>}></Route>
-        <Route path="/reset-password" element={<ConfirPassword/>}></Route>
+        <Route path="/reset-password" element={<ConfirmPassword/>}></Route>
 
 
       </Routes>
